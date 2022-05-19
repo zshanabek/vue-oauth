f27330819bf3517f3ed308491a40e8fc5d09312f
Zhunisali <zshanabek@gmail.com>
2022-05-19 17:42:40 +0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,126 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <div class="container">
+      <div class="columns" style="margin-top: 100px;">
+        <div class="column col-4 centered">
+        <div v-if="!user">
+          <button @click="handleClickSignIn">sign in</button>
+        </div>
+        <div v-else>
+          <button @click="handleClickGetAuthCode" :disabled="!Vue3GoogleOauth.isInit">get authCode</button>
+          <button @click="handleClickSignOut" :disabled="!Vue3GoogleOauth.isAuthorized">sign out</button>
+          <button @click="handleClickDisconnect" :disabled="!Vue3GoogleOauth.isAuthorized">disconnect</button>
+          <user-panel :user="user"></user-panel>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { inject, toRefs } from "vue";
+import axios from 'axios'
+import UserPanel from '@/components/UserPanel'
 
 export default {
-  name: 'App',
+  name: "HelloWorld",
   components: {
-    HelloWorld
-  }
-}
+    UserPanel,
+  },
+  props: {
+    msg: String,
+  },
+  data(){
+    return {
+      user: null,
+    }
+  },
+  methods: {
+    async handleClickSignIn(){
+      try {
+        const googleUser = await this.$gAuth.signIn();
+        if (!googleUser) {
+          return null;
+        }
+        const token = googleUser.xc.access_token
+        axios.post('http://localhost:8000/dj-rest-auth/google/', {
+          access_token: token
+        })
+          .then(resp => {
+            this.user = resp.data.user
+          })
+          .catch(err => {
+            console.log(err.response)
+          })
+      } catch (error) {
+        //on fail do something
+        console.error(error);
+        return null;
+      }
+    },
+    async handleClickGetAuthCode(){
+      try {
+        const authCode = await this.$gAuth.getAuthCode();
+        console.log("authCode", authCode);
+      } catch(error) {
+        //on fail do something
+        console.error(error);
+        return null;
+      }
+    },
+    async handleClickSignOut() {
+      try {
+        await this.$gAuth.signOut();
+        console.log("isAuthorized", this.Vue3GoogleOauth.isAuthorized);
+        this.user = "";
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    handleClickDisconnect() {
+      window.location.href = `https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=${window.location.href}`;
+    },
+  },
+  setup(props) {
+    const { isSignIn } = toRefs(props);
+    const Vue3GoogleOauth = inject("Vue3GoogleOauth");
+    const handleClickLogin = () => {};
+    return {
+      Vue3GoogleOauth,
+      handleClickLogin,
+      isSignIn,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+button {
+  display: inline-block;
+  line-height: 1;
+  white-space: nowrap;
+  cursor: pointer;
+  background: #fff;
+  border: 1px solid #dcdfe6;
+  color: #606266;
+  -webkit-appearance: none;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  outline: 0;
+  margin: 0;
+  -webkit-transition: 0.1s;
+  transition: 0.1s;
+  font-weight: 500;
+  padding: 12px 20px;
+  font-size: 14px;
+  border-radius: 4px;
+  margin-right: 1em;
+}
+button:disabled {
+  background: #fff;
+  color: #ddd;
+  cursor: not-allowed;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import gAuthPlugin from 'vue3-google-oauth2';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+let gauthClientId = "712407789741-m2f1g999bvqth0t7gbfv4b49qvgutdf2.apps.googleusercontent.com";
+
+const gauthOption = {
+    clientId: gauthClientId,
+    scope: 'profile email',
+    prompt: 'select_account',
+    plugin_name: 'chat'
+};
+  
+app.use(gAuthPlugin, gauthOption)
+
+app.mount('#app')
